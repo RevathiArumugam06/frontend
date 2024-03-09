9687cf198e0bd910b011715314e781d69e326b8a
RevathiArumugam06 <revathiarumugam454@gmail.com>
2024-03-09 03:47:17 -0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.alphansotech.com%2FFrontend%2Fimg%2Feventz_landing%2Fsection_one_img.png&tbnid=1JPegK0etakTpM&vet=12ahUKEwixsMPsiueEAxWFyjgGHQswAegQMygIegQIARBg..i&imgrefurl=https%3A%2F%2Fwww.alphansotech.com%2Fopen-source-event-ticketing-system&docid=4x9LrduWUPMi1M&w=909&h=559&q=event%20management%20open%20source%20image&hl=en-GB&ved=2ahUKEwixsMPsiueEAxWFyjgGHQswAegQMygIegQIARBg" alt="Welcome picture" />
+    </div>
+  )
+}
+
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API = "https://backend-2rmy.vercel.app"
